681a49c42690885da8bd9af7ab8e50629b688ecf
z337-408 <54790070+z337-408@users.noreply.github.com>
2019-09-04 15:54:27 +0800

    Add files via upload

---
+++ car196_train.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export CUDA_VISIBLE_DEVICES=0,1
+
+python train.py --dataset car --n_classes 196 --base_lr 1e-3 --batch_size 128 --epoch 200 --drop_rate 0.2 --T_k 10 --weight_decay 1e-8 --step 1
+
+sleep 300
+
+python train.py --dataset car --n_classes 196 --base_lr 1e-4 --batch_size 64 --epoch 100 --drop_rate 0.2 --T_k 10 --weight_decay 1e-5 --step 2
